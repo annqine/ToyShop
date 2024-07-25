31dd23ec18248e1cf9b7080620fa112f1e194cb0
annqine <143606594+annqine@users.noreply.github.com>
2024-07-25 22:19:01 +0300

    fix header

--- app/Views/header.php
+++ app/Views/header.php
@@ -5,13 +5,13 @@
   <title>All Toys</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="../public/css/style.css">
+  <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
   <header class="p-3 mb-3 border-bottom">
     <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+      <div class="d-flex flex-wrap align-items-center justify-content-between">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
           <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
             <use xlink:href="#bootstrap"></use>
@@ -19,28 +19,22 @@
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 link-secondary">Overview</a></li>
-          <li><a href="admin.php" class="nav-link px-2 link-body-emphasis">admin</a></li>
-          <li></li><a href="/admin">Перейти в админку</a></li>
+          <li><a href="/admin" class="nav-link px-2 link-body-emphasis">Login</a></li>
           <li><a href="#" class="nav-link px-2 link-body-emphasis">Customers</a></li>
           <li><a href="#" class="nav-link px-2 link-body-emphasis">Products</a></li>
         </ul>
 
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
-        </form>
-
-        <div class="dropdown text-end">
-          <ul class="dropdown-menu text-small">
-            <li><a class="dropdown-item" href="#">New project...</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
-            <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#">Sign out</a></li>
-          </ul>
+        <div class="d-flex align-items-center">
+          <div class="input-group mb-3 search-group">
+            <input type="text" class="form-control" placeholder="Search" aria-label="Search">
+            <button class="btn btn-search" type="button">
+              <img src="/images/search-icon.png" alt="Search">
+            </button>
+          </div>
         </div>
       </div>
     </div>
   </header>
+</body>
+
+</html>
--- app/Views/home.php
+++ app/Views/home.php
@@ -9,72 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
-    <style>
-        .card-img-top {
-            width: 100%;
-            height: 200px;
-            /* Фиксированная высота */
-            object-fit: cover;
-            /* Обрезка по центру */
-            object-position: center;
-            /* Центрирование изображения */
-        }
-
-        .card-title {
-            font-size: 1rem;
-            font-weight: 500;
-            line-height: 1.2;
-            height: 3.6em;
-            /* Высота для трех строк */
-            display: -webkit-box;
-            -webkit-box-orient: vertical;
-            -webkit-line-clamp: 3;
-            overflow: hidden;
-            text-align: center;
-            word-wrap: break-word;
-            text-overflow: ellipsis;
-        }
-
-        .price-button-container {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            margin-top: 1rem;
-        }
-
-        .price {
-            margin-bottom: 0;
-            font-size: 0.875rem;
-            font-weight: 600;
-        }
-
-        .btn-cart {
-            padding: 0.5rem;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            background-color: #ff69b4;
-            /* Розовый цвет */
-            color: white;
-            border: none;
-            border-radius: 0.25rem;
-            cursor: pointer;
-        }
-
-        .btn-cart img {
-            width: 20px;
-            height: 20px;
-        }
-
-        .btn-cart:hover {
-            background-color: #ff1493;
-            /* Темно-розовый цвет при наведении */
-        }
-
-        .card-body {
-            text-align: center;
-        }
-    </style>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
@@ -95,9 +30,13 @@
                                 <h5 class="card-title"><?php echo htmlspecialchars($toy['name_toys']); ?></h5>
                                 <div class="price-button-container">
                                     <p class="price">Price: <?php echo htmlspecialchars($toy['price']); ?></p>
-                                    <button class="btn-cart">
-                                        <img src="/images/cart-icon.png" alt="Cart">
-                                    </button>
+                                    <a href="#" class="btn-cart <?php echo empty($cartImage) ? 'no-img' : ''; ?>">
+                                        <?php if (!empty($cartImage)): ?>
+                                            <img src="<?php echo htmlspecialchars($cartImage); ?>" alt="Cart">
+                                        <?php else: ?>
+                                            <span>Cart</span> <!-- Текст, если изображение отсутствует -->
+                                        <?php endif; ?>
+                                    </a>
                                 </div>
                             </div>
                         </div>
