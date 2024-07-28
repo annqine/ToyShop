d12662a45157a8d9cc69c1c5da8bab930e45953b
annqine <143606594+annqine@users.noreply.github.com>
2024-07-28 20:57:01 +0300

    adding details

--- app/Views/header.php
+++ app/Views/header.php
@@ -12,11 +12,11 @@
   <header class="p-3 mb-3 border-bottom">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-between">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+        <!-- <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
           <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
             <use xlink:href="#bootstrap"></use>
           </svg>
-        </a>
+        </a> -->
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li><a href="/admin" class="nav-link px-2 link-body-emphasis">Login</a></li>
@@ -25,7 +25,7 @@
         </ul>
 
         <div class="d-flex align-items-center">
-          <form class="input-group mb-3 search-group" method="get" action="/search">
+          <form class="input-group search-group" method="get" action="/search">
             <input type="text" name="query" class="form-control" placeholder="Search" aria-label="Search">
             <button class="btn btn-search" type="submit">
               <img src="/images/search-icon.png" alt="Search">
